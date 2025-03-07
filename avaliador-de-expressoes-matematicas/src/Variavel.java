41cf00f830e16eb96cfbb056dc8a33da85a91840
CarlosDinizz <10444407@mackenzista.com.br>
2025-03-07 11:33:48 +0000

    Criação da classe Variavel

---
+++ avaliador-de-expressoes-matematicas/src/Variavel.java
@@ -0,0 +1,31 @@
+
+
+public class Variavel {
+    
+    private Character letra;
+    
+    private Integer valor;
+
+    public Variavel(){}
+
+    public Variavel(Character letra, Integer valor){
+        this.letra = letra;
+        this.valor = valor;
+    }
+
+    public Character getLetra(){
+        return letra;
+    }
+
+    public Integer getValor(){
+        return valor;
+    }
+
+    public void setLetra(Character letra){
+        this.letra = letra;
+    }
+
+    public void setValor(Integer valor){
+        this.valor = valor;
+    }
+}
